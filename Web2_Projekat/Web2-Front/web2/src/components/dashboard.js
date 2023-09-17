c933edab9a5962a1675686218443e551b0f56446
Milena Cancar <milenaacancar@gmail.com>
2023-09-17 13:53:50 +0200

    Popravljen dashboard da radi prema rolama

--- Web2_Projekat/Web2-Front/web2/src/components/dashboard.js
+++ Web2_Projekat/Web2-Front/web2/src/components/dashboard.js
@@ -1,7 +1,10 @@
 import { Box, UnorderedList, ListItem, Button } from "@chakra-ui/react";
 import { Link as RouterLink } from "react-router-dom";
+import AuthContext from "../contexts/auth-context";
+import { useContext } from "react";
 
 const Dashboard = (props) => {
+  const context = useContext(AuthContext);
   return (
     <Box flex={1} display="flex">
       <Box flex={1} bg="gray.300" flexDir="column" p="2rem">
@@ -16,62 +19,101 @@ const Dashboard = (props) => {
               </Button>
             </RouterLink>
           </ListItem>
-          <ListItem>
-            <Button
-              _hover={{ backgroundColor: "transparent" }}
-              bg="transparent"
-            >
-              Dodavanje artikla
-            </Button>
-          </ListItem>
-          <ListItem>
-            <Button
-              _hover={{ backgroundColor: "transparent" }}
-              bg="transparent"
-            >
-              Nova porudžbina
-            </Button>
-          </ListItem>
-          <ListItem>
-            <Button
-              _hover={{ backgroundColor: "transparent" }}
-              bg="transparent"
-            >
-              Prethodne porudžbine
-            </Button>
-          </ListItem>
-          <ListItem>
-            <Button
-              _hover={{ backgroundColor: "transparent" }}
-              bg="transparent"
-            >
-              Verifikacija
-            </Button>
-          </ListItem>
-          <ListItem>
-            <Button
-              _hover={{ backgroundColor: "transparent" }}
-              bg="transparent"
-            >
-              Nove Porudžbine
-            </Button>
-          </ListItem>
-          <ListItem>
-            <Button
-              _hover={{ backgroundColor: "transparent" }}
-              bg="transparent"
-            >
-              Moje porudžbina
-            </Button>
-          </ListItem>
-          <ListItem>
-            <Button
-              _hover={{ backgroundColor: "transparent" }}
-              bg="transparent"
-            >
-              Sve porudžbine
-            </Button>
-          </ListItem>
+
+          {context.type() === "Administrator" && (
+            <>
+              <ListItem>
+                <RouterLink to="/Verifications">
+                  <Button
+                    _hover={{ backgroundColor: "transparent" }}
+                    bg="transparent"
+                  >
+                    Verifikacija
+                  </Button>
+                </RouterLink>
+              </ListItem>
+              <ListItem>
+                <RouterLink to="/AllOrders">
+                  <Button
+                    _hover={{ backgroundColor: "transparent" }}
+                    bg="transparent"
+                  >
+                    Sve porudžbine
+                  </Button>
+                </RouterLink>
+              </ListItem>
+            </>
+          )}
+
+          {context.type() === "Seller" && (
+            <>
+              <ListItem>
+                <RouterLink to="/Products">
+                  <Button
+                    _hover={{ backgroundColor: "transparent" }}
+                    bg="transparent"
+                  >
+                    Proizvodi
+                  </Button>
+                </RouterLink>
+              </ListItem>
+              <ListItem>
+                <RouterLink to="/NewOrders">
+                  <Button
+                    _hover={{ backgroundColor: "transparent" }}
+                    bg="transparent"
+                  >
+                    Nove porudzbine
+                  </Button>
+                </RouterLink>
+              </ListItem>
+              <ListItem>
+                <RouterLink to="/MyOrders">
+                  <Button
+                    _hover={{ backgroundColor: "transparent" }}
+                    bg="transparent"
+                  >
+                    Moje porudzbine
+                  </Button>
+                </RouterLink>
+              </ListItem>
+              <ListItem>
+                <RouterLink to="/Map">
+                  <Button
+                    _hover={{ backgroundColor: "transparent" }}
+                    bg="transparent"
+                  >
+                    Mapa
+                  </Button>
+                </RouterLink>
+              </ListItem>
+            </>
+          )}
+
+          {context.type() === "Buyer" && (
+            <>
+              <ListItem>
+                <RouterLink to="/BuyerNewOrder">
+                  <Button
+                    _hover={{ backgroundColor: "transparent" }}
+                    bg="transparent"
+                  >
+                    Nove porudzbine
+                  </Button>
+                </RouterLink>
+              </ListItem>
+              <ListItem>
+                <RouterLink to="/PreviousOrders">
+                  <Button
+                    _hover={{ backgroundColor: "transparent" }}
+                    bg="transparent"
+                  >
+                    Prethodne porudzbine
+                  </Button>
+                </RouterLink>
+              </ListItem>
+            </>
+          )}
         </UnorderedList>
       </Box>
       <Box bg="gray.100">{props.children}</Box>
